dee4f0012896b6145fda6d8dfe4108d498ecfa4e
Moez Ben Hassen <37988307+MoezBenHassen@users.noreply.github.com>
2025-05-25 20:53:09 +0100

    pre perl update for rollback

--- output/test/rollback_V15.14.0.0.0.11__COL-52636.sql
+++ output/test/rollback_V15.14.0.0.0.11__COL-52636.sql
@@ -1,8 +1,7 @@
 -- 🔁 Rollback of V15.14.0.0.0.11__COL-52636.sql
 -- ⚠️ MANUAL CHECK REQUIRED: EXECUTE IMMEDIATE using unknown or uncaptured variable v_select_sequence_sql
 -- ORIGINAL: EXECUTE IMMEDIATE v_select_sequence_sql INTO s_count;
--- ⚠️ MANUAL CHECK REQUIRED: EXECUTE IMMEDIATE using unknown or uncaptured variable v_select_next_val_sql
--- ORIGINAL: EXECUTE IMMEDIATE v_select_next_val_sql INTO v_next_val;
+-- ⚠️ Unrecognized EXECUTE IMMEDIATE rollback from v_select_next_val_sql: SELECT CommonRefData_SEQ.NEXTVAL FROM DUAL
 -- ⚠️ Unrecognized EXECUTE IMMEDIATE rollback from v_select_max_id_sql: SELECT nvl(MAX (id),0) FROM RefData
 DELETE FROM refdata WHERE id = 'v_next_val' AND scheme = 'STATUS_AMP' AND refdatavalue = 'Rejected' AND description = 'Rejected' AND status = 11 ;
 DELETE FROM refdata WHERE id = 'v_next_val' AND scheme = 'STATUS_AMP' AND refdatavalue = 'Substitution Accepted' AND description = 'Substitution Accepted' AND status = 11 ;
@@ -25,6 +24,10 @@ DELETE FROM refdata WHERE id = 'v_next_val' AND scheme = 'STATUS_AMP' AND refdat
 DELETE FROM refdata WHERE id = 'v_next_val' AND scheme = 'STATUS_AMP' AND refdatavalue = 'Created Issued' AND description = 'Created Issued' AND status = 11 ;
 DELETE FROM refdata WHERE id = 'v_next_val' AND scheme = 'STATUS_AMP' AND refdatavalue = 'Amended Initiated' AND description = 'Amended Initiated' AND status = 11 ;
 DELETE FROM refdata WHERE id = 'v_next_val' AND scheme = 'STATUS_AMP' AND refdatavalue = 'Amended Issued' AND description = 'Amended Issued' AND status = 11 ;
+DELETE FROM refdata WHERE id = 'v_next_val' AND scheme = 'CANCELCODES_AMP' AND refdatavalue = 9201 AND description = 'Replacement Collateral Unavailable' AND status = 11 AND flag = 0 AND category = 0 AND substitutioncanceled = 1 ;
+DELETE FROM refdata WHERE id = 'v_next_val' AND scheme = 'CANCELCODES_AMP' AND refdatavalue = 9202 AND description = 'Requested Collateral Inaccurate' AND status = 11 AND flag = 0 AND category = 0 AND substitutioncanceled = 1 ;
+DELETE FROM refdata WHERE id = 'v_next_val' AND scheme = 'CANCELCODES_AMP' AND refdatavalue = 9203 AND description = 'Asset Recall Not Required' AND status = 11 AND flag = 0 AND category = 0 AND substitutioncanceled = 1 ;
+DELETE FROM refdata WHERE id = 'v_next_val' AND scheme = 'CANCELCODES_AMP' AND refdatavalue = 9204 AND description = 'Duplicate Request' AND status = 11 AND flag = 0 AND category = 0 AND substitutioncanceled = 1 ;
 -- ⚠️ MANUAL CHECK REQUIRED: This DELETE statement needs careful review for rollback.
 -- ORIGINAL: DELETE FROM RefData WHERE SCHEME = 'STATUS_AMP' AND refDataValue ='Substitution Reject Initiated';
 -- ⚠️ MANUAL CHECK REQUIRED: This DELETE statement needs careful review for rollback.
